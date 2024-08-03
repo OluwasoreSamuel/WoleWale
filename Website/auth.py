2d1311d86e77a23421875bf36d90fca30c6bb1e6
oluwasoresamuel <oluwasoresamuel@gmail.com>
2024-08-03 02:11:48 +0100

    contacts and other htmls

--- Website/auth.py
+++ Website/auth.py
@@ -1,5 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
+@auth.route('/about_us')
+def login():
+    return render_template("about.html")
 
+@auth.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+@auth.route('/contact')
+def contact():
+    return render_template('contact.html')
